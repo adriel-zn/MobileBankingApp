aa2f42d36b4ec8ad68fb590a4fa15fe3fcba40f2
Adriel Mariappen <adriel.mariappen@jas.com>
2025-02-13 19:00:11 +0200

    Implement display for beneficiaries

--- MobileBankingApp/BankingApp.MAUI/MainPage.xaml.cs
+++ MobileBankingApp/BankingApp.MAUI/MainPage.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace BankingApp.MAUI
+﻿using BankingApp.MAUI.ViewModels;
+
+namespace BankingApp.MAUI
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new BeneficiaryViewModel();
         }
     }
 
