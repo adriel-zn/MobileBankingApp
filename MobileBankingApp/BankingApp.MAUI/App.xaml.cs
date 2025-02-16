0735e61296a46476ed3b37a9bf54c16d97876668
Adriel Mariappen <adriel.mariappen@jas.com>
2025-02-16 16:27:08 +0200

    Exception handler in progress.

--- MobileBankingApp/BankingApp.MAUI/App.xaml.cs
+++ MobileBankingApp/BankingApp.MAUI/App.xaml.cs
@@ -6,6 +6,23 @@
         {
             InitializeComponent();
 
+            // Subscribe to unhandled exception events
+            //AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
+            //{
+            //    var ex = e.ExceptionObject as Exception;
+            //    if (ex != null)
+            //    {
+            //        // Log the exception or show an alert
+            //        Console.WriteLine($"Unhandled Exception: {ex.Message}");
+
+            //        // Optionally, show an alert or handle the exception globally
+            //        MainThread.BeginInvokeOnMainThread(async () =>
+            //        {
+            //            await Shell.Current.DisplayAlert("Unexpected Error", ex.Message, "OK");
+            //        });
+            //    }
+            //};
+
             MainPage = new AppShell();
         }
     }
